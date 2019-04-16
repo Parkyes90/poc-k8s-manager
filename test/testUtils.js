2756eb34250f262df8052f18c011454f0916ca9f
parkyes90 <parkyes90@gmail.com>
2019-04-16 16:52:18 +0900

    install Redux Thunk

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,7 +1,8 @@
 import checkPropTypes from "check-prop-types";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 import rootReducer from '../src/reducers';
+import { middlewares } from "../src/configureStore";
 
 /**
  *
@@ -9,7 +10,8 @@ import rootReducer from '../src/reducers';
  * @returns {Store<any, Action> & *}
  */
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
 /**
  *
