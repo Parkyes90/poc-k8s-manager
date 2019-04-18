c51e1927115ebd7e0a6329911137e2b988078712
parkyes90 <parkyes90@gmail.com>
2019-04-18 15:33:28 +0900

    Connect App component and tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,73 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
 
-test('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { findByTestAttr, storeFactory } from "../test/testUtils";
+import Input from "./Input";
+
+/**
+ *
+ * @param initialState
+ */
+const setup = (initialState = {}) => {
+  const store = storeFactory(initialState);
+  return shallow(<Input store={store} />)
+    .dive()
+    .dive();
+};
+
+describe("render", () => {
+  describe("word has not been guessed", () => {
+    let wrapper;
+
+    beforeEach(() => {
+      const initialState = { success: false };
+      wrapper = setup(initialState);
+    });
+    test("renders component without error", () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+    test("renders input box", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(1);
+    });
+    test("renders submit button", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(1);
+    });
+  });
+  describe("word has been guessed", () => {
+    let wrapper;
+
+    beforeEach(() => {
+      const initialState = { success: true };
+      wrapper = setup(initialState);
+    });
+    test("renders component without error", () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+    test("does not render input box", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(0);
+    });
+    test("does not render submit button", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(0);
+    });
+  });
+});
+
+describe("redux props", () => {
+  test("has success piece of state as props", () => {
+    const success = true;
+    const wrapper = setup({ success });
+    const successProp = wrapper.instance().props.success;
+    expect(successProp).toBe(success);
+  });
+  test("guessWord action creator is a function props", () => {
+    const wrapper = setup();
+    const guessWordProp = wrapper.instance().props.guessWord;
+    expect(guessWordProp).toBeInstanceOf(Function);
+  });
 });
